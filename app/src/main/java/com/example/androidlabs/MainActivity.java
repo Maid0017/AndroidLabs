b736e279cc6af5e9c335007662774b0d11fb7de0
RyanMaidment <maidment.ryan@gmail.com>
2021-01-25 18:48:13 -0500

    commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.androidlabs;
 
+import android.os.Bundle;
+import android.widget.CheckBox;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+
+    CheckBox cb =(CheckBox)findViewById(R.id.chckbox);
+    cb.setOnCheckedChangeListener( ((compoundButton, b) -> {
+        Snackbar.make(cb,"Yo Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Undo", click -> compoundButton.setChecked(!b))
+                .show();
+        }));
     }
+
 }
