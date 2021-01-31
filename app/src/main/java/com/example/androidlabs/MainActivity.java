ab6bb5c965411e60c2925c90d515ac5850a943db
RyanMaidment <maidment.ryan@gmail.com>
2021-01-31 18:53:15 -0500

    commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.androidlabs;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +16,40 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
+    private EditText emailEdit;
+    private EditText passwordEdit;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_email);
 
-    CheckBox cb =(CheckBox)findViewById(R.id.chckbox);
-    cb.setOnCheckedChangeListener( ((compoundButton, b) -> {
-        Snackbar.make(cb,"Yo Snackbar", Snackbar.LENGTH_LONG)
-                .setAction("Undo", click -> compoundButton.setChecked(!b))
-                .show();
-        }));
+    passwordEdit = (EditText) findViewById(R.id.passwordEdit);
+    emailEdit = (EditText) findViewById(R.id.emailEdit);
+    loginButton = (Button) findViewById(R.id.loginButton);
+
+    mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+    mEditor = mPreferences.edit();
+    checkSharedPreferences();
+    loginButton.setOnClickListener(view -> {
+        String email = emailEdit.getText().toString();
+        mEditor.putString(getString(R.string.email), email);
+        mEditor.commit();
+
+        String password = passwordEdit.getText().toString();
+        mEditor.putString(getString(R.string.password), password);
+        mEditor.commit();
+    });
+    
     }
+    private void checkSharedPreferences() {
+        String email = mPreferences.getString(getString(R.string.email), "");
+        String password = mPreferences.getString(getString(R.string.password), "");
 
+        emailEdit.setText(email);
+        passwordEdit.setText(password);
+    };
 }
---
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -0,0 +1,74 @@
+package com.example.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageButton;
+public class ProfileActivity extends AppCompatActivity {
+
+    private ImageButton confirmImgBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        confirmImgBtn = (ImageButton) findViewById(R.id.confirmImgBtn);
+
+        confirmImgBtn.setOnClickListener(view -> {
+            dispatchTakePictureIntent();
+        });
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            confirmImgBtn.setImageBitmap(imageBitmap);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+    }
+}
